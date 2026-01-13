526530d9b24b76aed35c0284742530558a4d86e5
alam00000 <adullah.alam.tapadar2000@gmail.com>
2026-01-13 16:59:25 +0530

    fix: made bookmark pdf type safe

--- src/js/types/index.ts
+++ src/js/types/index.ts
@@ -46,3 +46,4 @@ export * from './pdf-to-zip-type.ts';
 export * from './sign-pdf-type.ts';
 export * from './add-watermark-type.ts';
 export * from './email-to-pdf-type.ts';
+export * from './bookmark-pdf-type.ts';
